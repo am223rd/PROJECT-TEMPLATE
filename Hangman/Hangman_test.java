67f9f697885d3d154b335e88c86d636be64135a6
am223rd <am223rd@student.lnu.se>
2019-04-05 21:47:14 +0200

    Create Hangman_test.java

---
+++ Hangman/Hangman_test.java
@@ -0,0 +1,21 @@
+package Hangman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Hangman_test {
+
+	@Test
+	void testGameMain() {
+
+	}
+
+	
+
+	@Test
+	void testgetSecretWord() {
+		fail("Not yet implemented");
+	}
+
+}
